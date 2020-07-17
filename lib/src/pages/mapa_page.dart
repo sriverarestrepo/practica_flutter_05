0baeea55af615bf97407f0b0141e63b72e721e59
santrire <srivera@ias.com.co>
2020-07-16 22:16:23 -0500

    Se cambia version de flutter_map, se adiciona floatingBottom para realizar cambio aleatorio de estilo de mapa

--- lib/src/pages/mapa_page.dart
+++ lib/src/pages/mapa_page.dart
@@ -11,6 +11,10 @@ class MapaPage extends StatefulWidget {
 }
 
 class _MapaPageState extends State<MapaPage> {
+
+  MapController mapController = new MapController();
+  String mapStyle = 'satellite';
+
   @override
   Widget build(BuildContext context) {
 
@@ -23,22 +27,24 @@ class _MapaPageState extends State<MapaPage> {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.my_location), 
-            onPressed: (){}
+            onPressed: (){
+              mapController.move(scan.getLatLng(), 15);
+            }
           )
         ],
       ),
       body: _crearFlutterMap(scan),
+      floatingActionButton: _crearBotonFlotante(context),
     );
   }
 
   Widget _crearFlutterMap(ScanModel scan) {
 
-
     return FlutterMap(
+      mapController: mapController,
       options: MapOptions(
         center: scan.getLatLng(),
         zoom: 15,
-        maxZoom: 20,
       ),
       layers: [
         _crearMapa(),
@@ -50,18 +56,20 @@ class _MapaPageState extends State<MapaPage> {
   }
 
   _crearMapa() {
+
     return TileLayerOptions(
       urlTemplate: 'https://api.mapbox.com/v4/'
           '{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}',
       additionalOptions: {
       'accessToken':'pk.eyJ1Ijoic3JycGx1cyIsImEiOiJja2NlMjJnODYwMjNkMnprMmwwdmR3MXlxIn0.3CztlFdk3LsM-nN3sHe-vA',
-      'id': 'mapbox.satellite'
+      'id': 'mapbox.$mapStyle'
       // streets , dark, light, outdoors, satellite
       },
     );
   }
 
   _crearMarcadores(ScanModel scan) {
+
     return MarkerLayerOptions(
       markers: <Marker> [
         Marker(
@@ -78,6 +86,29 @@ class _MapaPageState extends State<MapaPage> {
         ),
 
       ]
+
+    );
+  }
+
+  Widget _crearBotonFlotante(BuildContext context) {
+
+    return FloatingActionButton(
+      child: Icon(Icons.repeat),
+      backgroundColor: Theme.of(context).primaryColor,
+      onPressed: () {
+        
+        if(mapStyle == 'satellite'){
+          mapStyle = 'mapbox-streets-v8';
+        }else if(mapStyle == 'mapbox-streets-v8'){
+          mapStyle = 'mapbox-streets-v6';
+        }else if (mapStyle == 'mapbox-streets-v6'){
+          mapStyle = 'mapbox-streets-v7';
+        }else {
+          mapStyle = 'satellite';
+        }
+       
+       setState(() { });
+      }
 
     );
   }
